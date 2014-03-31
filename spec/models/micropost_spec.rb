e1d9fa57a0778c333d655171b66412d8036fe332
Elena <lenulja87@mail.ru>
2014-03-31 14:56:38 +0400

    relationship_spec, micropost_spec

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -2,30 +2,30 @@ require 'spec_helper'
 
 describe Micropost do
 
-  let(:user) { FactoryGirl.create(:user) }
-  before { @micropost = user.microposts.build(content: "Lorem ipsum") }
+  let(:user) { FactoryGirl.create(:user) } # тащит все данные юзера
+  before { @micropost = user.microposts.build(content: "Lorem ipsum") } # ???
 
   subject { @micropost }
 
-  it { should respond_to(:content) }
-  it { should respond_to(:user_id) }
-  it { should respond_to(:user) }
-  its(:user) { should eq user }
+  it { should respond_to(:content) } # имеет сам контент
+  it { should respond_to(:user_id) } # имеет идентификатор юзера
+  it { should respond_to(:user) } # имеет данные юзера
+  its(:user) { should eq user } # ???
 
-  it { should be_valid }
+  it { should be_valid } # валидно
 
-  describe "when user_id is not present" do
+  describe "when user_id is not present" do # описывает "Если ID юзера нет"
     before { @micropost.user_id = nil }
-    it { should_not be_valid }
+    it { should_not be_valid } # не валидно
   end
 
-  describe "with blank content" do
-    before { @micropost.content = " " }
-    it { should_not be_valid }
+  describe "with blank content" do # описывает "Пустое поле сообщения"
+    before { @micropost.content = " " } # поле пустое
+    it { should_not be_valid } # не валидно
   end
 
-  describe "with content that is too long" do
-    before { @micropost.content = "a" * 141 }
-    it { should_not be_valid }
+  describe "with content that is too long" do # описывает "Длинное сообщение"
+    before { @micropost.content = "a" * 141 } # текст длиннее 140 символов
+    it { should_not be_valid } # не валидно
   end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,28 +1,28 @@
 require 'spec_helper'
 
-describe Relationship do
+describe Relationship do # описание связей
 
-  let(:follower) { FactoryGirl.create(:user) }
-  let(:followed) { FactoryGirl.create(:user) }
-  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+  let(:follower) { FactoryGirl.create(:user) } # подписанные юзеры подтягивают данные о юзерах
+  let(:followed) { FactoryGirl.create(:user) } # юзеры, на которых подписан пользователь подтягивают данные о себе
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) } # не понятно
 
-  subject { relationship }
+  subject { relationship } # субьект Связи
 
-  it { should be_valid }
+  it { should be_valid } # валидно
 
-  describe "follower methods" do
-    it { should respond_to(:follower) }
-    it { should respond_to(:followed) }
-    its(:follower) { should eq follower }
-    its(:followed) { should eq followed }
+  describe "follower methods" do # описание "Методы фолловер"
+    it { should respond_to(:follower) } # должно реагировать на фолловер
+    it { should respond_to(:followed) } # должно реагировать на фолловед
+    its(:follower) { should eq follower } #  вытягивает данные подписанных пользователей из подписанных
+    its(:followed) { should eq followed } # аналогично предыдущей строке
   end
 
-  describe "when followed id is not present" do
+  describe "when followed id is not present" do # описание "Когда нет ID фолловера"
     before { relationship.followed_id = nil }
-    it { should_not be_valid }
+    it { should_not be_valid } # не валидно
   end
 
-  describe "when follower id is not present" do
+  describe "when follower id is not present" do # аналогично
     before { relationship.follower_id = nil }
     it { should_not be_valid }
   end
